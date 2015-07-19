c73a8f4d19c894b662535d409b4c9fa1559f87a6
Gabriella Lubliner <gabriella.lubliner@gmail.com>
2015-07-19 16:19:45 -0400

    trying to fix port issue

--- app.js
+++ app.js
@@ -9,9 +9,7 @@ var path = require('path');
 var app = express();
 
 app.set('port', process.env.PORT || 80);
-var server = app.listen(app.get('port'), function() {
-	debug('Express server listening on port ' + + server.address().port);
-});
+var server = app.listen(app.get('port'));
 var io = require('socket.io').listen(server);
 
 
